891784271f8a73a0a0db777c0fd5526b2c78af80
ning yiming <ning.yiming@outlook.com>
2023-06-04 10:44:28 +0800

    20230604

---
+++ 11.py
@@ -0,0 +1,26 @@
+print("两个黄鹂鸣翠柳，",end="")
+print("一行白鹭上青天。",end="")
+print("窗含西岭千秋雪，",end="")
+print("门泊东吴万里船。",end="")
+
+
+#2
+lst=["张飞","关羽","刘备","诸葛亮","赵子龙","曹操"]
+print()
+print(lst[0],end="")
+print(lst[1])
+print(lst[2],end="")
+print(lst[3])
+print(lst[4],end="")
+print(lst[5])
+
+#3
+a=["te", "ae", "ew", "hr","qw","hh", "jt"]
+a.pop(4)
+print(a)
+b=["te", "ae", "ew", "hr","qw","hh", "jt"]
+b[4]=None
+print(b)
+
+#4
+c="江南好，风景旧曾。日出江花红胜火，春来江水绿如蓝。能不忆江南啊？"
---
+++ 练习1.py
@@ -0,0 +1,4 @@
+a=["print","len","beak","continue","input","while"]
+a.remove("beak")
+
+#2题
